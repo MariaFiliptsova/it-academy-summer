e96c7f129c38bd7c6af277be468349e859fe8c94
Maria Filiptsova <m.filiptsova@gmail.com>
2020-06-25 17:04:28 +0200

    fix length of the line

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -81,7 +81,8 @@ if __name__ == '__main__':
 
 
 '''A group of friends want to buy a bouquet of flowers.
-The florist wants to maximize his number of new customers and the money he makes.
+The florist wants to maximize his number of new customers
+and the money he makes.
 To do this, he decides he'll multiply the price of each flower by the number
 of that customer's previously purchased flowers plus 1.
 The first flower will be original price, (0 + 1) x original price,
