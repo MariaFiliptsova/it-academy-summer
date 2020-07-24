2366612ff8af7a1c6057b8ba75fa3b89ea6dded8
Maria Filiptsova <m.filiptsova@gmail.com>
2020-07-24 13:19:46 +0200

    Fix tests errors

--- src/Homework4/Slova.py
+++ src/Homework4/Slova.py
@@ -17,5 +17,3 @@ tekst = 'Пьер сидел в гостиной, где Шиншин, как с
 punc = string.punctuation
 tekst_without_punc = ''.join(symbol for symbol in tekst if symbol not in punc)
 print(len(set(tekst_without_punc.split())))
-
-
