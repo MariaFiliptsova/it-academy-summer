f564d276e1e4cd00f54af11a3dca9a07ab07872d
Maria Filiptsova <m.filiptsova@gmail.com>
2020-07-28 11:02:30 +0300

    Add changes according to comments

--- src/Homework5/IMDB.py
+++ src/Homework5/IMDB.py
@@ -10,36 +10,39 @@ line_starts_with_new = False
 count_movies = 0
 counter = {}
 y_counter = {}
+movies_250 = 250
+a = open('ratings.list', 'r', encoding="ISO-8859-1")
+b = open('top250_movies.txt', 'w')
+c = open('ratings.txt', 'w')
+d = open('years.txt', 'w')
 try:
-    with open('ratings.list', 'r', encoding="ISO-8859-1") as f:
-        with open('top250_movies.txt', 'w') as top250:
-            with open('ratings.txt', 'w') as movie_ratings:
-                with open('years.txt', 'w') as years:
+    with a as f, b as top250:
+        with c as movie_ratings, d as years:
+            for line in f:
+                if line.startswith('New'):
+                    line_starts_with_new = True
                     for line in f:
-                        if line.startswith('New'):
-                            line_starts_with_new = True
-                            for line in f:
-                                if line_starts_with_new is True:
-                                    if count_movies < 250:
-                                        name = line.split()[3:-1]
-                                        top250.write(' '.join(name) + '\n')
-                                        rating = line.split()[2]
-                                        if rating not in counter:
-                                            counter.update({rating: 1})
-                                        else:
-                                            counter[rating] += 1
-                                        year = line.split()[-1]
-                                        if year not in y_counter:
-                                            y_counter.update({year: 1})
-                                        else:
-                                            y_counter[year] += 1
-                                        count_movies += 1
-                    list_keys = list(y_counter.keys())
-                    list_keys.sort()
-                    for k in counter:
-                        movie_ratings.write(k + ' ' + '*' * counter[k] + '\n')
-                    for y in list_keys:
-                        x = y.strip('()')
-                        years.write(x + ' ' + '*' * y_counter[y] + '\n')
+                        if line_starts_with_new is True:
+                            if count_movies < movies_250:
+                                name = line.split()[3:-1]
+                                top250.write(' '.join(name) + '\n')
+                                rating = line.split()[2]
+                                if rating not in counter:
+                                    counter.update({rating: 1})
+                                else:
+                                    counter[rating] += 1
+                                year = line.split()[-1]
+                                if year not in y_counter:
+                                    y_counter.update({year: 1})
+                                else:
+                                    y_counter[year] += 1
+                                count_movies += 1
+            list_keys = list(y_counter.keys())
+            list_keys.sort()
+            for k in counter:
+                movie_ratings.write(k + ' ' + '*' * counter[k] + '\n')
+            for y in list_keys:
+                x = y.strip('()')
+                years.write(x + ' ' + '*' * y_counter[y] + '\n')
 except FileNotFoundError:
     print('File Not Found')
--- src/Homework5/Runner.py
+++ src/Homework5/Runner.py
@@ -7,60 +7,60 @@ runner() – все фукнции вызываются по очереди
 runner(‘func_name’) – вызывается только функцию func_name.
 runner(‘func’, ‘func1’...) - вызывает все переданные функции
 '''
-'''#Дан список чисел. Посчитайте, сколько в нем пар элементов,
-равных друг другу. Считается, что любые два элемента,
-равные друг другу образуют одну пару, которую необходимо посчитать.
-Входные данные - строка из чисел, разделенная пробелами.
-Выходные данные - количество пар.
-Важно: 1 1 1 - это 3 пары, 1 1 1 1 - это 6 пар'''
 
+class Functions():
+    # Дан список чисел. Посчитайте, сколько в нем пар элементов,
+    # равных друг другу. Считается, что любые два элемента,
+    # равные друг другу образуют одну пару, которую необходимо посчитать.
+    # Входные данные - строка из чисел, разделенная пробелами.
+    # Выходные данные - количество пар.
+    # Важно: 1 1 1 - это 3 пары, 1 1 1 1 - это 6 пар
 
-def pairs(spisok=[1, 1, 1]):
-    count_appearences = {}
-    number_of_pairs = 0
-    for i in spisok:
-        count_appearences.update({i: spisok.count(i)})
+    def pairs(spisok=[1, 1, 1]):
+        count_appearences = {}
+        number_of_pairs = 0
+        for i in spisok:
+            count_appearences.update({i: spisok.count(i)})
         for j in count_appearences.values():
             if j > 1:
-                b_c = math.factorial(j) / \
-                    (math.factorial(2) * math.factorial(j - 2))
+                j_fact = math.factorial(j)
+                b_c = j_fact / (math.factorial(2) * math.factorial(j - 2))
                 number_of_pairs += b_c
                 return number_of_pairs
 
+    # Дан список целых чисел.
+    # Требуется переместить все ненулевые элементы в левую часть списка,
+    # не меняя их порядок, а все нули - в правую часть.
+    # Порядок ненулевых элементов изменять нельзя,
+    # дополнительный список использовать нельзя,
+    # задачу нужно выполнить за один проход по списку.
+    # Распечатайте полученный список.
 
-'''Дан список целых чисел.
-Требуется переместить все ненулевые элементы в левую часть списка,
-не меняя их порядок, а все нули - в правую часть.
-Порядок ненулевых элементов изменять нельзя,
-дополнительный список использовать нельзя,
-задачу нужно выполнить за один проход по списку.
-Распечатайте полученный список.
-'''
-
-
-def ordered(data=[0, 1, 0, 3]):
-    for i in data:
-        if i == 0:
-            data.remove(i)
-            data.append(i)
-    return data
+    def ordered(data=[0, 1, 0, 3]):
+        for i in data:
+            if i == 0:
+                data.remove(i)
+                data.append(i)
+        return data
 
 
 def runner(*args):
     if len(args) == 0:
-        print(pairs())
-        print(ordered())
+        for obj in dir(Functions):
+            if obj.startswith('__'):
+                pass
+            else:
+                all_func = getattr(Functions, obj)
+                print(all_func())
     else:
         for arg in args:
-            if arg == 'pairs':
-                print(pairs())
-            elif arg == 'ordered':
-                print(ordered())
+            if arg in dir(Functions):
+                func = getattr(Functions, arg)
+                print(func())
             else:
                 print('Unknown function')
-
 
 runner()
 runner('pairs')
 runner('ordered', 'pairs')
-runner('func')
+runner('fun')
