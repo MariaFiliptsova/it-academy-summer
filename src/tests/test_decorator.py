aebaaea3c5a99363272eca3da6177027d17a0d02
Maria Filiptsova <m.filiptsova@gmail.com>
2020-08-19 09:37:24 +0200

    fix errors v3

--- src/tests/test_decorator.py
+++ src/tests/test_decorator.py
@@ -15,7 +15,7 @@ class DecoratorTest(unittest.TestCase):
     )
     @ddt.unpack
     def test_sum(self, res, expected):
-        '''Assert that the homework.func correctly sums up 2 numbers'''
+        """Assert that the homework.func correctly sums up 2 numbers"""
         self.assertEqual(res, expected)
 
     @ddt.data(
@@ -25,14 +25,17 @@ class DecoratorTest(unittest.TestCase):
     )
     @ddt.unpack
     def test_error(self, first_p, second_p):
-        '''Assert that TypeError is raised in case of wrong data type'''
+        """Assert that TypeError is raised in case of wrong data type"""
         with self.assertRaises(TypeError):
             homework.func(first_p, second_p)
 
     def test_filecontent(self):
-        '''Assert that after homework.py is run the file results.txt has
+
+        """Assert that after homework.py is run the file results.txt has
         the correct data
-        '''
+        
+        """
+
         with open('results.txt', 'r') as f:
             for line in f:
                 line_content = ' '.join(line.split()[1:3])
